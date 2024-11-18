3816e69dfda330f689eeb0631792b60cdf88d6ff
giangdn <giangsd4@gmail.com>
2024-11-18 13:38:57 +0700

    update done ui home page

--- src/app/ui/components/home/Backer/index.tsx
+++ src/app/ui/components/home/Backer/index.tsx
@@ -6,7 +6,7 @@ interface Props {
 
 const Backer: FC<Props> = ({ title }) => {
   return (
-    <div className="border-b border-solid border-[#185BEA] bg-[#fff] mobile:border-b-0 mobile:py-[45px]">
+    <div className="border-b border-solid border-[#185BEA] bg-[#fff] mobile:border-b-0 mobile:py-[0px]">
       <div className="mx-auto flex max-w-[1200px] items-center justify-around mobile:hidden mobile:flex-col">
         <div className="w-[40%] whitespace-pre-line py-[80px] font-bold text-[30px] capitalize leading-9 text-[#231F20]">
           {title}
@@ -19,7 +19,7 @@ const Backer: FC<Props> = ({ title }) => {
         </div>
       </div>
       <div className="hidden mobile:block">
-        <div className="whitespace-pre-line px-[20px] py-[40px] text-center font-bold text-[30px] capitalize leading-9 text-[#231F20]">
+        <div className="whitespace-pre-line px-[20px] py-[40px] text-center font-bold text-[30px] capitalize leading-9 text-[#231F20] mobile:text-[23px]">
           {title}
         </div>
         <div className="flex border-b border-t border-[#185BEA]">
