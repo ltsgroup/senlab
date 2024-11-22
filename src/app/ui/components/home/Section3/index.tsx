6b1f4354215e65b3060e2c581d7385cd789e9396
CUNGVANTHANG <cungvanthang2k3@gmail.com>
2024-11-22 15:24:26 +0700

    update ui

--- src/app/ui/components/home/Section3/index.tsx
+++ src/app/ui/components/home/Section3/index.tsx
@@ -48,7 +48,7 @@ const arrSlide = [
 const Section3: FC<Props> = ({titleMobile, title, desc}) => {
   return (
     <div className="flex items-center justify-center bg-[#FFE8B2]">
-      <div className="mx-auto max-w-[1200px] py-[100px] mobile:p-[20px] mobile:py-[45px]">
+      <div className="mx-auto max-w-[1200px] overflow-hidden py-[100px] mobile:p-[20px] mobile:pt-[45px] mobile:pb-[25px]">
         <div className="mb-[70px] mobile:mb-[35px]">
           <h1 className="mobile:hidden whitespace-pre-line text-center font-bold text-[50px] leading-[55px] text-[#231F20] mobile:text-center mobile:text-[26px] mobile:leading-[30px]">
             {title}
@@ -60,8 +60,7 @@ const Section3: FC<Props> = ({titleMobile, title, desc}) => {
             {desc}
           </p>
         </div>
-       
-        <div className="flex items-center justify-center gap-[30px] mobile:flex-col mobile:gap-[30px]">
+        <div className="mobile:hidden flex items-center justify-center gap-[30px] mobile:flex-row mobile:gap-[15px]">
           {arrSlide?.map((item) => (
             <div
               key={item.name}
@@ -91,6 +90,50 @@ const Section3: FC<Props> = ({titleMobile, title, desc}) => {
               </div>
             </div>
           ))}
+        </div>
+        <div className="hidden mobile:block">
+          <Swiper
+            modules={[Pagination]}
+            pagination={{clickable: true}}
+            spaceBetween={"220px"}
+            slidesPerView={"auto"}
+            centeredSlides={true}
+            loop={true}
+            className="h-[450px] custom-pagination"
+          >
+            {arrSlide?.map((item) => (
+              <SwiperSlide
+                key={item.name}
+                className="flex items-center justify-center"
+                style={{ width: "220px" }}
+              >
+                <div className="flex h-[405px] w-full flex-col justify-between bg-white p-4 shadow-[8px_8px_0px_0px_#EB1773]">
+                  <div>
+                    <Image
+                      alt={item.name}
+                      src={item.img}
+                      className="mb-5 aspect-[189/208] w-[189px]"
+                    />
+                    <div className="mb-4 rounded border-[1px] border-[#000] bg-[#EB1773] text-center font-medium text-[18px] text-white">
+                      {item.name}
+                    </div>
+                    <div className="h-[40px] text-center font-mono text-[16px] leading-5 text-[#231F20]">
+                      {item.pos}
+                    </div>
+                  </div>
+                  <div className="relative mt-[24px] flex justify-center">
+                    <div className="absolute top-[50%] z-0 h-[0.5px] w-[70%] -translate-y-1/2 bg-[#3f3f3f]"></div>
+                    <Link
+                      href={item.linkin}
+                      className="relative z-[1] w-[32px] bg-white p-[6px]"
+                    >
+                      <Image src={linkedin} alt={"linkedin-logo"} />
+                    </Link>
+                  </div>
+                </div>
+              </SwiperSlide>
+            ))}
+          </Swiper>
         </div>
       </div>
     </div>
